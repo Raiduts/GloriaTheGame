a485eea64972cf1d5385bd1405a13dc144082716
Raiduts <rivaldorakun1253@gmail.com>
2023-03-23 14:04:25 +0700

    change level
    udah bisa change level

---
+++ Assets/ToLevel1.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToLevel1 : MonoBehaviour
+{
+
+    public int levelIndeks;
+
+    private void OnTriggerEnter2D(Collider2D player){
+        if(player.tag == "Player"){
+            SceneManager.LoadScene(levelIndeks, LoadSceneMode.Single);
+        }
+    }
+}
